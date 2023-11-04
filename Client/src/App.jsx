ee4f4064663edb82ffbd4b4e4985d50047711583
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-11-04 13:25:16 +0600

    initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+//Components import
+import Login from './components/account/Login'
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/components/account/Login.jsx
@@ -0,0 +1,30 @@
+
+
+import Blog from "../../assets/Blog.jpg"
+
+import "./login.css"
+
+const Login = () => {
+
+   
+    return (
+        <div className="container">
+           <div className="login-box">
+                <div className="input-form">
+                    <h1 className="login-heading">Login</h1>
+                    <input className="login-input-field"  placeholder="Enter Your Email"/>
+                    <input className="login-input-field"  placeholder="Enter Your Password"/>
+                    <div className="login-buttons">
+                        <button className="login-btn">Login</button>
+                        <button className="login-btn">Create account</button>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src={Blog} className="login-image" alt="Blog image" />
+                </div>
+           </div>
+        </div>
+    );
+};
+
+export default Login;
