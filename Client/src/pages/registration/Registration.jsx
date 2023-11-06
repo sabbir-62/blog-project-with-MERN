992912afd875c9f0dea3fb3a4b53fc7928fcad95
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-11-06 19:36:15 +0600

    [In progress]Registration page update

--- Client/src/pages/registration/Registration.jsx
+++ Client/src/pages/registration/Registration.jsx
@@ -1,23 +1,43 @@
+//Imports Required components
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Blog from "../../assets/Blog.jpg"
-
 import "./registration.css"
 
-const Registration = () => {
 
+/*----------Registration Component----------*/
+const Registration = () => {
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        cpassword: ""
+    })
+
+    //Set form value into state
+    const setValues = (key, value) => {
+        setState({
+            ...state,
+            [key]: value
+        })
+    }
+
+
+    //Navigate login page
     const navigate = useNavigate()
     const loginPage = () => {
         navigate('/login')
     }
+    //Return JSX
     return (
         <div className="registration-container">
            <div className="registration-box">
                 <div className="input-form">
                     <h1 className="registration-heading">Registration</h1>
-                    <input className="registration-input-field"  placeholder="Enter Your Name"/>
-                    <input className="registration-input-field"  placeholder="Enter Your Email"/>
-                    <input className="registration-input-field"  placeholder="Enter Your Password"/>
-                    <input className="registration-input-field"  placeholder="Confirm Password"/>
+                    <input className="registration-input-field" placeholder="Enter Your Name" onChange={(e)=>setValues("name", e.target.value)}/>
+                    <input className="registration-input-field" placeholder="Enter Your Email" onChange={(e)=>setValues("email", e.target.value)}/>
+                    <input className="registration-input-field" placeholder="Enter Your Password" onChange={(e)=>setValues("password", e.target.value)}/>
+                    <input className="registration-input-field" placeholder="Confirm Password" onChange={(e)=>setValues("cpassword", e.target.value)}/>
                     <div className="registration-buttons">
                         <button className="registration-btn btn">Submit</button>
                         <button className="registration-btn create-account-btn btn" onClick={loginPage}>Already a user</button>
