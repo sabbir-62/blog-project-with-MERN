986e7e4a57e343cb13191d81069ada9ef3b0c30f
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-11-08 13:14:05 +0600

    [In progress] database connection successfully

---
+++ Server/database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connection = async () => {
+    const db = process.env.DATABASE;
+
+    try {
+        await mongoose.connect(db);
+        
+        console.log("Database Connection Success");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = connection;
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connection = require('./database/db')
 
 const app = express();
 
 const PORT = 8000;
+
+
+connection()
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
